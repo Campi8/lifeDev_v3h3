22abb1559d825c9c6a49fc772219565f07faada3
Campi8 <gustavocampi02@gmail.com>
2023-11-06 22:38:16 -0300

    aula 06-11 css e register

--- lifedev/src/pages/Register/Register.jsx
+++ lifedev/src/pages/Register/Register.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const [displayName, setDisplayName] = useState('')
+const [emailusuario, setEmailUsuario] = useState('')
+const [senhausuario, setsenhaUsuario] = useState('')
+const [Confirmação, setConfirmacao] = useState('')
+const [erro, setErro] = useState('')
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div>
+      <h1>Cadastro de LifeDeveopers</h1>
+      <form>
+
+        <label>
+          <span>Nome: </span>
+          <input type="text" name='displayName' placeholder='Digite o Nome' required/>
+        </label>
+
+        <label>
+          <span>Email: </span>
+          <input type="email" name='emailusuario' placeholder='Digite seu e-mail' required/>
+        </label>
+
+        <label>
+          <span>Senha: </span>
+          <input type="password" name='senhausuario' placeholder='entre com uma senha segura' required/>
+        </label>
+
+        <label>
+          <span>Confirmação: </span>
+          <input type="password" name='senhausuario' placeholder='Confirme a senha' required/>
+        </label>
+
+        <button className='btn'>
+          Cadastrar
+        </button>
+
+      </form>
+    </div>
   )
 }
 
